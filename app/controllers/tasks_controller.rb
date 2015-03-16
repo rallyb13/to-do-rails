d17b6b76f14ed16cef4aa1a5852239204fc02160
Benjamin Herson <benjaminherson@gmail.com>
2015-03-16 14:48:14 -0700

    add RESTful routes for tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,45 @@
+class TasksController < ApplicationController
+
+  def create
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.new(task_params)
+    if @task.save
+      flash[:notice] = "Task successfully added!"
+      redirect_to list_path(@task.list)
+    else
+      render :new
+    end
+  end
+
+  def new
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.new
+  end
+
+
+  def edit
+    @task = Task.find(params[:id])
+    @list = @task.list
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(params[:task])
+      redirect_to list_path(@task.list)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to list_path(@task.list)
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:description)
+    end
+
+end
